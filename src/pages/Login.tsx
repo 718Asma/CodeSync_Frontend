30206ea4ee7c723c51fa5a0d01fb1372700944f2
noel_vi <krichenyassine22@gmail.com>
2024-05-08 00:02:37 +0100

    login fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "../utils/axios";
 import { useNavigate } from "react-router-dom";
 import redirector from "../utils/redirector";
@@ -6,59 +6,99 @@ import redirector from "../utils/redirector";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 
-const Login = () => {
-    function GoogleLoginButton() {
-        const onSuccess = async (response: any) => {
-            if (!response) console.error("onSuccess response is null.");
-            const decodedCredential = jwtDecode(response.credential);
-            const googleUser = {
-                googleId: response.clientId,
-                fullName: (decodedCredential as any).name,
-            };
-            const res = await axios.post("/auth/google", googleUser);
-            const { refresh_token, access_token } = res.data;
-            localStorage.setItem("access_token", access_token);
-            localStorage.setItem("refresh_token", refresh_token);
-            navigate("/");
-        };
+interface LoginResponse {
+    access_token: string;
+    refresh_token: string;
+    user_id: string;
+}
 
-        const onFail = () => {
-            console.error("Login failed:");
-        };
+const setLocalStorage = (data: LoginResponse) => {
+    localStorage.setItem("access_token", data.access_token);
+    localStorage.setItem("refresh_token", data.refresh_token);
+    localStorage.setItem("user_id", data.user_id);
+};
 
-        return <GoogleLogin onSuccess={onSuccess} onError={() => onFail()} />;
-    }
-
+const GoogleLoginButton: React.FC = () => {
     const navigate = useNavigate();
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState<string[]>([]);
-
-    async function handleLogin(e: any) {
-        e.preventDefault();
-        setError([]);
-        if (username === "")
-            setError((error) => [...error, "Username must not be empty."]);
-        if (password === "")
-            setError((error) => [...error, "Password must not be empty."]);
-        let user = {
-            username: username,
-            password: password,
+    const onSuccess = async (res: any) => {
+        if (!res) console.error("'onSuccess' response is null.");
+        const decodedRes = jwtDecode(res.credential);
+        const loginBody = {
+            googleId: decodedRes.sub,
+            fullName: (decodedRes as any).name,
         };
         try {
-            const response = await axios.post("/auth/login", user);
+            const { data } = await axios.post("/auth/google", loginBody);
+            setLocalStorage(data);
+            navigate("/");
+        } catch (err) {
+            console.error("an error occurred with the login request: ", err);
+        }
+    };
 
-            const { refresh_token, access_token, user_id } = response.data;
-            localStorage.setItem("access_token", access_token);
-            localStorage.setItem("refresh_token", refresh_token);
-            localStorage.setItem("user_id", user_id);
+    const onFail = () => {
+        console.error("google login failed.");
+    };
 
+    return <GoogleLogin onSuccess={onSuccess} onError={onFail} />;
+};
+
+const Login = () => {
+    const navigate = useNavigate();
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const [error, setError] = useState<string[]>([]);
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError([]);
+        const username = usernameRef.current?.value;
+        const password = passwordRef.current?.value;
+        if (!username)
+            setError((prevErrors) => [
+                ...prevErrors,
+                "Username must not be empty.",
+            ]);
+        if (!password)
+            setError((prevErrors) => [
+                ...prevErrors,
+                "Password must not be empty.",
+            ]);
+
+        if (username && username.length < 3)
+            setError((prevErrors) => [
+                ...prevErrors,
+                "Username must be at least 3 characters long.",
+            ]);
+
+        if (password && password.length < 8)
+            setError((prevErrors) => [
+                ...prevErrors,
+                "Password must be at least 8 characters long.",
+            ]);
+
+        if (error.length > 0) return;
+
+        let loginBody = {
+            username: username!,
+            password: password!,
+        };
+        try {
+            const { data } = await axios.post<LoginResponse>(
+                "/auth/login",
+                loginBody
+            );
+            setLocalStorage(data);
             navigate("/");
         } catch (error) {
             console.error("Error: ", error);
+            setError((prevErrors) => [
+                ...prevErrors,
+                (error as any).response.data.message,
+            ]);
         }
-    }
+    };
 
     useEffect(() => {
         redirector(navigate);
@@ -79,12 +119,11 @@ const Login = () => {
                         Username:
                     </label>
                     <input
+                        ref={usernameRef}
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         type="text"
                         id="username"
                         name="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
                         placeholder="Enter your username"
                     />
                 </div>
@@ -96,12 +135,11 @@ const Login = () => {
                         Password:
                     </label>
                     <input
+                        ref={passwordRef}
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                         type="password"
                         id="password"
                         name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
                         placeholder="Enter your password"
                     />
                 </div>
@@ -109,17 +147,19 @@ const Login = () => {
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="submit"
-                        onClick={(e) => handleLogin(e)}
+                        onClick={handleLogin}
                     >
                         Login
                     </button>
                 </div>
             </form>
-            <ul className="text-red-500">
-                {error.map((err, index) => (
-                    <li key={index}>{err}</li>
-                ))}
-            </ul>
+            {error.length > 0 && (
+                <ul className="text-red-500 bg-red-100 p-2 rounded-md mb-4">
+                    {error.map((err, index) => (
+                        <li key={index}>{err}</li>
+                    ))}
+                </ul>
+            )}
 
             <div className="text-center mt-4">
                 <p>Didn't sign up yet?</p>
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -9,7 +9,6 @@ axiosInstance.interceptors.request.use(async (config) => {
 
     if (access_token) {
         config.headers.authorization = `Bearer ${access_token}`;
-        console.log("Access token set in axios instance: ", access_token);
     }
 
     return config;
@@ -24,7 +23,6 @@ axiosInstance.interceptors.response.use(
         if (error.response.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
             const refresh_token = localStorage.getItem("refresh_token");
-            console.log("Refresh token: ", refresh_token);
             try {
                 const response = await axiosInstance.post(
                     "/auth/refresh-token",
@@ -33,9 +31,7 @@ axiosInstance.interceptors.response.use(
                     }
                 );
                 const { access_token } = response.data;
-                console.log("New access token: ", access_token);
                 localStorage.setItem("access_token", access_token);
-                console.log("Retry original request with new access token");
                 return axiosInstance(originalRequest);
             } catch (error) {
                 console.error("Error refreshing token: ", error);
