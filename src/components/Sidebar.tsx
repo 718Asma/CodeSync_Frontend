a1e451af97f137db7a4e0482aaa4358ad5acb503
Anis AttouAni <anisattouani17@gmail.com>
2024-05-22 21:09:37 +0100

    anis2

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignIn } from "@fortawesome/free-solid-svg-icons";
+import { faSignIn, faSignOut } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from 'react-router-dom';
 
 const Sidebar = ( ) => {
@@ -16,30 +16,30 @@ const Sidebar = ( ) => {
     };
 
     return (
-        <aside className="p-7 w-1/6 side">
+        <aside className="w-1/6 side">
             <div>
-                <div className="flex items-center">
+                <div className="flex items-center mx-7 mt-7">
                     <img src="../assets/ooga.png" alt="Profile pic" className="mr-2" style={{ height: '50px', width: '50px' }} />
                     <div>
-                        <p style={{ fontWeight: 'bold', marginBottom: '0' }}>{user}</p>
+                        <p className='font-semibold'>{user}</p>
                         <button
                             className="info"
                             onClick={handleLogout}
                             style={{ color: '#ED080B' }}
                         >
-                            <FontAwesomeIcon icon={faSignIn} />&nbsp;
+                            <FontAwesomeIcon icon={faSignOut} />&nbsp;
                             Log Out
                         </button>
                     </div>
                 </div>
-                <p className="info">Hi, I am a graphic designer</p>
+                <p className="info mx-7">Hi, I am a graphic designer</p>
                 <br/>
                 <div className="flex items-center mb-4">
-                    <p className="follow"><span>427</span> Followers</p>
-                    <p className="follow"><span>845</span> Following</p>
+                    <p className="follow w-full"><span>427</span> Followers</p>
+                    <p className="follow w-full"><span>845</span> Following</p>
                 </div>
             </div>
-                <div>
+                <div className='mx-7'>
                     <p style={{fontWeight : 'bold', fontSize:'20px'}}>Trending Topics</p>
                     <ol>
                         <li><a>#Programming</a></li>
@@ -53,7 +53,7 @@ const Sidebar = ( ) => {
                     </ol>
                 </div>
             <br/>
-            <div>
+            <div className='mx-7'>
                 <p style={{fontWeight : 'bold', fontSize:'20px'}}>Friends</p>
                 
             </div>
